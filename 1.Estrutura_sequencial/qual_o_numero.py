103dd387e410d7427a996d589d9c65218658e328
sftool <sftool13@gmail.com>
2019-08-09 16:33:38 -0300

    atual

--- 1.Estrutura_sequencial/qual_o_numero.py
+++ 1.Estrutura_sequencial/qual_o_numero.py
@@ -1,5 +1,5 @@
 # Mostra o numero
 numero = int(input("\nDigite um numero "))
-print("\nO numero digitado foi: ", numero)
+print("\nO numero digitado foi: \033[33m", numero)
 
-enter = input("\nPressione <Enter> para encerrar...")
+enter = input("\033[00m\nPressione <Enter> para encerrar...")
