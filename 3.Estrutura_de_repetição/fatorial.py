e0b1e6d2986625ceaaff0ba5ef0adfeb11445d6c
sftool <sftool13@gmail.com>
2019-08-08 22:20:46 -0300

    foi

--- 3.Estrutura_de_repetição/fatorial.py
+++ 3.Estrutura_de_repetição/fatorial.py
@@ -1,6 +1,6 @@
 from math import factorial
 n = int(input("\nDigite um número: "))
 f = factorial(n)
-print("\nO fatorial de {} é {}.".format(n, f))
+print("\nO fatorial de {} é: \n\n{}.".format(n, f))
 
 enter = input("\nPressione <Enter> para encerrar...")
