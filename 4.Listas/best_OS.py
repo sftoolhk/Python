582261d7d7ba6a791e18e9d0e605e1931fecfa3e
sftool <sftool13@gmail.com>
2019-08-09 17:18:28 -0300

    pastas

---
+++ 4.Listas/best_OS.py
@@ -0,0 +1,6 @@
+1 - Windows Server
+2 - Unix
+3 - Linux
+4 - Netware
+5 - MacOS
+6 - Outro
---
+++ 4.Listas/rebuild_mouse.py
@@ -0,0 +1,4 @@
+a. Necessita de esfera;
+b. Necessita de limpeza;
+c. Necessita troca de cabo;
+d. Conector quebrado ou inutilizado
