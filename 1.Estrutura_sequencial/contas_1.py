a350b6a084cce9f1083ef8b7432281234be2fbc1
sftool <sftool13@gmail.com>
2019-08-08 21:15:39 -0300

    edy

--- 1.Estrutura_sequencial/contas_1.py
+++ 1.Estrutura_sequencial/contas_1.py
@@ -6,7 +6,7 @@ num3 = float(input("Entre com um numero real: "))
 produto = (num1 * 2) * (num2 / 2)
 soma = (num1 * 3) + num3
 elevado = num3 ** 3
-print("\nO produto do dobro do primeiro e metade do segundo é: %s" % produto)
+print("\nO produto do dobro do primeiro e metade do segundo eé: %s" % produto)
 print("\nA soma do triplo do primeiro com o terceiro é: %s" % soma)
 print("\nO terceiro numero elevado ao cubo é: %s" % elevado)
 
